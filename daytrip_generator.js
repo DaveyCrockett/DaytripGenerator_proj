f384ad2464dcadcd4c0e9a17634f791c7a6ea649
DAVID paredes <augustspies22@gmail.com>
2021-04-02 11:38:54 -0700

    Added a for loop for prompt and cleaned up else if statement.

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -33,45 +33,21 @@ let destinations = ["California", "Hawaii", "Virginia", "Florida"];
         console.log(randoEntertainment);
     }
 
+for(let index = 0; index < 3; index++){
 let userInput = prompt("Are you Happy or Sad with choices.")
     
 if(userInput == "Happy"){
     console.log("Day Trip is complete")
+    break;
 }else if(userInput == "Sad"){
     
-    destinations = ["California", "Hawaii", "Virginia", "Florida"];
-
-    function RandomDestination(selectDestination){
-        let randoDestination = selectDestination[Math.floor(Math.random() * selectDestination.length)];
-        console.log(randoDestination);
-    }
-
     RandomDestination(destinations);
-
-    restaurant = ["Taco Bell", "Lazy Dog", "Waffle House", "Burger King"];
-
-    function RandomRestaurant(selectRestaurant){
-        let randoRestaurant = selectRestaurant[Math.floor(Math.random() * selectRestaurant.length)];
-        console.log(randoRestaurant);
-    }
 
     RandomRestaurant(restaurant);
 
-    transportation = ["Sedan", "SUV", "Truck", "Bus", "Train"];
-
-    function RandomTransportation(selectTransportation){
-        let randoTransportation = selectTransportation[Math.floor(Math.random() * selectTransportation.length)];
-        console.log(randoTransportation);
-    }
-
     RandomTransportation(transportation);
 
-    entertainment = ["Watch a Movie", "See a Play", "Go Rock Climbing", "Go skateboarding", "Go Snowboarding"];
-
-    function RandomEntertainment(selectEntertainment){
-        let randoEntertainment = selectEntertainment[Math.floor(Math.random() * selectEntertainment.length)];
-        console.log(randoEntertainment);
-    }
-
     RandomEntertainment(entertainment);
+    
+}
 }
