4a0c1fc5946972188cdf11ca7cb03557928e4538
DAVID paredes <augustspies22@gmail.com>
2021-04-02 09:10:50 -0700

    New Random Restaurant function and new random Transportation function added

--- daytrip_generator.js
+++ daytrip_generator.js
@@ -4,7 +4,25 @@ let destinations = ["California", "Hawaii", "Virginia", "Florida"];
 
 function RandomDestination(selectDestination){
     let randoDestination = selectDestination[Math.floor(Math.random() * selectDestination.length)];
-    console.log(randoDestination)
+    console.log(randoDestination);
 }
 
 RandomDestination(destinations);
+
+let restaurant = ["Taco Bell", "Lazy Dog", "Waffle House", "Burger King"];
+
+function RandomRestaurant(selectRestaurant){
+    let randoRestaurant = selectRestaurant[Math.floor(Math.random() * selectRestaurant.length)];
+    console.log(randoRestaurant);
+}
+
+RandomRestaurant(restaurant);
+
+let transportation = ["Sedan", "SUV", "Truck", "Bus", "Train"];
+
+function RandomTransportation(selectTransportation){
+    let randoTransportation = selectTransportation[Math.floor(Math.random() * selectTransportation.length)];
+    console.log(randoTransportation);
+}
+
+RandomTransportation(transportation);
